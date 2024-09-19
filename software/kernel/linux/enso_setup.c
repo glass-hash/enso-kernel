87c32b9d6300e09783edf61e4757de3002a1784d
Kshitij Rana <kshitijr@andrew.cmu.edu>
2024-09-18 21:31:23 -0400

    Updated/added function headers in driver code; Ran pre-commit

--- software/kernel/linux/enso_setup.c
+++ software/kernel/linux/enso_setup.c
@@ -1,34 +1,67 @@
+/*
+ * Copyright (c) 2024, Carnegie Mellon University
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted (subject to the limitations in the disclaimer
+ * below) provided that the following conditions are met:
+ *
+ *      * Redistributions of source code must retain the above copyright notice,
+ *      this list of conditions and the following disclaimer.
+ *
+ *      * Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in the
+ *      documentation and/or other materials provided with the distribution.
+ *
+ *      * Neither the name of the copyright holder nor the names of its
+ *      contributors may be used to endorse or promote products derived from
+ *      this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY
+ * THIS LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT
+ * NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #include "enso_setup.h"
+
 #include "enso_chr.h"
 
 struct enso_global_bookkeep global_bk __read_mostly;
 
-// function defined in Intel's FPGA to get the BAR information
+// function defined in `intel_fpga_pcie_setup.c` to get the BAR information
 extern struct enso_intel_pcie* get_intel_fpga_pcie_addr(void);
 
 /******************************************************************************
  * Kernel Registration
  *****************************************************************************/
-/*
- * enso_init(void): Registers the Enso driver.
+/**
+ * @brief Registers the Enso driver.
  *
- * @returns: 0 if successful.
+ * @return 0 if successful, negative error code otherwise.
  *
  * */
 static __init int enso_init(void) {
   int ret;
-  struct dev_bookkeep *dev_bk;
+  struct dev_bookkeep* dev_bk;
 
   global_bk.intel_enso = NULL;
   global_bk.intel_enso = get_intel_fpga_pcie_addr();
-  if(global_bk.intel_enso == NULL) {
+  if (global_bk.intel_enso == NULL) {
     printk("Failed to receive BAR info\n");
     return -1;
   }
 
   // initialize a character device
   ret = enso_chr_init();
-  if(ret != 0) {
+  if (ret != 0) {
     printk("Failed to initialize character device\n");
     return ret;
   }
@@ -59,11 +92,10 @@ static __init int enso_init(void) {
   }
   global_bk.dev_bk = dev_bk;
   return 0;
-
 }
 module_init(enso_init);
 
-/*
+/**
  * enso_init(void): Unregisters the Enso driver.
  *
  * */
--- software/kernel/linux/enso_setup.h
+++ software/kernel/linux/enso_setup.h
@@ -1,24 +1,56 @@
+/*
+ * Copyright (c) 2024, Carnegie Mellon University
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted (subject to the limitations in the disclaimer
+ * below) provided that the following conditions are met:
+ *
+ *      * Redistributions of source code must retain the above copyright notice,
+ *      this list of conditions and the following disclaimer.
+ *
+ *      * Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in the
+ *      documentation and/or other materials provided with the distribution.
+ *
+ *      * Neither the name of the copyright holder nor the names of its
+ *      contributors may be used to endorse or promote products derived from
+ *      this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY
+ * THIS LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT
+ * NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #ifndef SOFTWARE_KERNEL_LINUX_ENSO_SETUP_H_
 #define SOFTWARE_KERNEL_LINUX_ENSO_SETUP_H_
 
+#include <asm/io.h>
+#include <linux/cdev.h>
+#include <linux/fs.h>
 #include <linux/kernel.h>
+#include <linux/mm.h>
 #include <linux/module.h>
 #include <linux/printk.h>
-#include <linux/cdev.h>
 #include <linux/slab.h>
-#include <linux/fs.h>
-#include <linux/version.h>
 #include <linux/uaccess.h>
-#include <linux/mm.h>
-#include <asm/io.h>
+#include <linux/version.h>
 
-#define ENSO_DRIVER_NAME        "enso"
-#define NOTIFICATION_BUF_SIZE   16384
-#define ENSO_PIPE_SIZE          32768
-#define MAX_TRANSFER_LEN        131072
-#define HUGE_PAGE_SIZE          (0x1ULL << 21)
-#define MEM_PER_QUEUE           (0x1ULL << 12)
-#define BATCH_SIZE              64
+#define ENSO_DRIVER_NAME "enso"
+#define NOTIFICATION_BUF_SIZE 16384
+#define ENSO_PIPE_SIZE 32768
+#define MAX_TRANSFER_LEN 131072
+#define HUGE_PAGE_SIZE (0x1ULL << 21)
+#define MEM_PER_QUEUE (0x1ULL << 12)
+#define BATCH_SIZE 64
 
 // These determine the maximum number of notification buffers and enso pipes.
 // These macros also exist in hardware and **must be kept in sync**. Update the
@@ -27,29 +59,28 @@
 #define MAX_NB_APPS 1024
 #define MAX_NB_FLOWS 8192
 
-/*
- * struct enso_intel_pcie
+/**
+ * @struct enso_intel_pcie
  *
- * @brief: Stores the PCIe Base Address Resgiter (BAR) information from
+ * @brief Stores the PCIe Base Address Resgiter (BAR) information from
  *         Intel's drivers. Note: we only use BAR2's information, rest are
  *         unused.
- *
  * */
 struct enso_intel_pcie {
-    void *__iomem base_addr;
+  void *__iomem base_addr;
 };
 
-/*
- * struct enso_global_bookkeep
+/**
+ * @struct enso_global_bookkeep
  *
- * @brief: Global bookkeeping information relevant to the Enso driver.
+ * @brief Global bookkeeping information relevant to the Enso driver.
  *
- * @cdev: Used to create a character device.
- * @chr_class: Used to creating the enso device under /dev/.
- * @intel_enso: The start of the BAR region to be mapped into the kernel space.
- * @chr_major: Major number of the device.
- * @chr_minor: Minor number of the device.
- *
+ * @var cdev Used to create a character device.
+ * @var chr_class Used to creating the enso device under /dev/.
+ * @var intel_enso The start of the BAR region to be mapped into the kernel
+ * space.
+ * @var chr_major Major number of the device.
+ * @var chr_minor Minor number of the device.
  * */
 struct enso_global_bookkeep {
   struct cdev cdev;
@@ -61,19 +92,20 @@ struct enso_global_bookkeep {
 };
 
 /**
- * struct dev_bookkeep - Bookkeeping structure per device.
+ * @struct dev_bookkeep
  *
- * @chr_open_cnt:   Number of character device handles opened with
+ * @brief Bookkeeping structure per device.
+ *
+ * @var chr_open_cnt   Number of character device handles opened with
  *                  this device selected.
- * @sem:            Synchronizes accesses to this structure. Can be used in
+ * @var sem            Synchronizes accesses to this structure. Can be used in
  *                  interrupt context. Currently, only @chr_open_cnt requires
  *                  synchronization as other fields are written once.
- * @nb_fb_queues:   Number of fallback queues.
- * @enable_rr:      Enable round-robin scheduling among fallback queues.
- * @notif_q_status: Bit vector to keep track of which notification queue has
+ * @var nb_fb_queues   Number of fallback queues.
+ * @var enable_rr     Enable round-robin scheduling among fallback queues.
+ * @var notif_q_status Bit vector to keep track of which notification queue has
  *                  been allocated.
- * @pipe_status:    Bit vector to keep track of which pipe has been allocated.
- *
+ * @var pipe_status   Bit vector to keep track of which pipe has been allocated.
  */
 struct dev_bookkeep {
   int chr_open_cnt;
@@ -85,19 +117,19 @@ struct dev_bookkeep {
 };
 
 /**
- * struct chr_dev_bookkeep - Bookkeeping structure per character device
- *                           handle.
+ * @struct chr_dev_bookkeep
  *
- * @dev_bk:         Pointer to the device bookkeeping structure for the
+ * @brief Bookkeeping structure per character device handle.
+ *
+ * @var dev_bk         Pointer to the device bookkeeping structure for the
  *                  currently selected device.
- * @nb_fb_queues:   Number of fallback queues.
- * @notif_q_status: Bit vector to keep track of which notification queue has
+ * @var nb_fb_queues   Number of fallback queues.
+ * @var notif_q_status Bit vector to keep track of which notification queue has
  *                  been allocated for this particular character device.
- * @pipe_status:    Bit vector to keep track of which pipe has been allocated
+ * @var pipe_status    Bit vector to keep track of which pipe has been allocated
  *                  for this particular character device.
- * @notif_buf_pair: Notification buffer pair specific to this device handle.
- * @enso_rx_pipes: Enso RX pipes specific to this device handle.
- *
+ * @var notif_buf_pair Notification buffer pair specific to this device handle.
+ * @var rx_pipes Enso RX pipes specific to this device handle.
  */
 struct chr_dev_bookkeep {
   struct dev_bookkeep *dev_bk;
@@ -108,6 +140,15 @@ struct chr_dev_bookkeep {
   struct rx_pipe_internal **rx_pipes;
 };
 
+/**
+ * @struct rx_notification
+ *
+ * @brief Represents the structure of an Rx notification.
+ *
+ * @var signal 1 for a valid Rx notification
+ * @var queue_id pipe ID of the enso pipe this notification belongs to
+ * @var tail TODO: figure this out
+ */
 struct __attribute__((__packed__)) rx_notification {
   uint64_t signal;
   uint64_t queue_id;
@@ -115,13 +156,45 @@ struct __attribute__((__packed__)) rx_notification {
   uint64_t pad[5];
 };
 
+/**
+ * @struct tx_notification
+ *
+ * @brief Represents the structure of a Tx notification. Also used
+ * to send configuration to the NIC.
+ *
+ * @var signal 1 for data, 2 for configuration.
+ * @var phys_addr starting address of the batch (physical address).
+ * @var length size of the batch in bytes (up to 1 MB).
+ */
 struct __attribute__((__packed__)) tx_notification {
   uint64_t signal;
   uint64_t phys_addr;
-  uint64_t length;  // In bytes (up to 1MB).
+  uint64_t length;
   uint64_t pad[5];
 };
 
+/**
+ * @struct queue_regs
+ *
+ * @brief Core structure representing a queue metadata of Rx and Tx
+ * notifications in a notification buffer pair structure. This structure is
+ * mapped into IO memory of the Enso NIC and configures the start Rx and Tx
+ * notification buffers (memory information, head, tail). Part of the
+ * notification_buf_pair structure.
+ *
+ * @var rx_tail tail of the Rx notification buffer.
+ * @var rx_head head of the Rx notification buffer.
+ * @var rx_mem_low contains the least significant 32 bits of the Rx buffer's
+ * physical address.
+ * @var rx_mem_high holds the most significant 32 bits of the Rx buffer's
+ * physical address.
+ * @var tx_tail tail of the Tx notification buffer.
+ * @var tx_head tail of the Rx notification buffer.
+ * @var tx_mem_low contains the least significant 32 bits of the Tx notification
+ * buffer's physical address.
+ * @var tx_mem_high contains the most significant 32 bits of the Tx notification
+ * buffer's physical address.
+ */
 struct queue_regs {
   uint32_t rx_tail;
   uint32_t rx_head;
@@ -134,40 +207,83 @@ struct queue_regs {
   uint32_t padding[8];
 };
 
+/**
+ * @struct notification_buf_pair
+ *
+ * @brief Represents a notification buffer pair (Rx and Tx).
+ *
+ * @var rx_buf Rx notification buffer (DMA allocated).
+ * @var tx_buf Tx notification buffer (DMA allocated).
+ * @var rx_head_ptr Points to the head of the Rx queue (tail is modified by the
+ * NIC).
+ * @var tx_tail_ptr Points to the tail of the Tx queue (head is modified by the
+ * NIC).
+ * @var regs queue_regs Storing Rx and Tx notification buffer metadata (memory
+ * mapped IO).
+ * @var next_rx_pipe_ids Next rx pipe id to consume, used in prefetching
+ * batches.
+ * @var wrap_tracker Keeps track of notifications where the packet wraps around
+ * the buffer's boundary.
+ * @var pending_rx_pipe_tails Stores the tail of the pipe (indexed by pipe ID).
+ * @var tx_full_cnt Number of times the Tx notification buffer was full.
+ * @var id Notification buffer ID.
+ * @var rx_head Used to store the value pointed by `rx_head_ptr`.
+ * @var tx_head Used to store the value of Tx notification buffer's head.
+ * @var tx_tail Used to store the value pointed by `tx_tail_ptr`.
+ * @var nb_unreported_completions Stores the number of unreported Tx
+ * notification completions to the application.
+ * @var next_rx_ids_head Stores the head of the next pipe ID to be fetched
+ * during prefetching.
+ * @var next_rx_ids_tail Stores the tail of the next pipe ID to be fetched
+ * during prefetching.
+ * */
 struct notification_buf_pair {
-  struct rx_notification* rx_buf;
-  uint32_t* next_rx_pipe_ids;  // Next pipe ids to consume from rx_buf.
-  struct tx_notification* tx_buf;
-  uint32_t* rx_head_ptr;
-  uint32_t* tx_tail_ptr;
+  struct rx_notification *rx_buf;
+  struct tx_notification *tx_buf;
+  uint32_t *rx_head_ptr;
+  uint32_t *tx_tail_ptr;
+  struct queue_regs *regs;
+  uint32_t *next_rx_pipe_ids;
+  uint8_t *wrap_tracker;
+  uint32_t *pending_rx_pipe_tails;
+
+  uint64_t tx_full_cnt;
+
+  uint32_t id;
   uint32_t rx_head;
   uint32_t tx_head;
   uint32_t tx_tail;
+  uint32_t nb_unreported_completions;
+
   uint16_t next_rx_ids_head;
   uint16_t next_rx_ids_tail;
-  uint32_t nb_unreported_completions;
-  uint32_t id;
-
-  struct queue_regs* regs;
-  uint64_t tx_full_cnt;
-  uint32_t ref_cnt;
-
-  uint8_t* wrap_tracker;
-  uint32_t* pending_rx_pipe_tails;
-
-  bool allocated;
 };
 
+/**
+ * struct rx_pipe_internal
+ *
+ * @brief Represents an Enso Rx pipe.
+ *
+ * @var regs queue_regs Stores Rx and Tx notification buffer metadata (memory
+ * mapped IO).
+ * @var buf_head_ptr pointer to the head of the Rx queue.
+ * @var rx_head stores the head of the Rx queue.
+ * @var rx_tail stores the tail of the Rx queue.
+ * @var id ID of the Rx pipe.
+ * @var allocated whether the Rx pipe is allocated or not.
+ * */
 struct rx_pipe_internal {
-  struct queue_regs* regs;
-  uint32_t* buf_head_ptr;
+  struct queue_regs *regs;
+  uint32_t *buf_head_ptr;
+
   uint32_t rx_head;
   uint32_t rx_tail;
   uint32_t id;
+
   bool allocated;
 };
 
 // extern as it is used by enso_chr.c to fill out the character device info.
 extern struct enso_global_bookkeep global_bk;
 
-#endif // SOFTWARE_KERNEL_LINUX_ENSO_SETUP_H_
+#endif  // SOFTWARE_KERNEL_LINUX_ENSO_SETUP_H_
