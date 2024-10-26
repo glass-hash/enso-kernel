52087306a5059d119fa55e4bdc322c45cb31499e
Kshitij Rana <kshitijr@andrew.cmu.edu>
2024-10-26 14:31:32 -0400

    Commented out per flow stats

--- software/src/enso/helpers.cpp
+++ software/src/enso/helpers.cpp
@@ -158,7 +158,7 @@ static void print_stats_line(uint64_t recv_bytes, uint64_t nb_batches,
 #define ONE_MILLION 1e6
 #define ONE_THOUSAND 1e3
 
-static void print_tx_flow_line(uint64_t delta_bytes, uint64_t delta_pkts) {
+/*static void print_tx_flow_line(uint64_t delta_bytes, uint64_t delta_pkts) {
   uint64_t tx_tput_mbps =
       (delta_bytes + delta_pkts * FPGA_PACKET_OVERHEAD) * 8. / (ONE_MILLION);
   uint64_t tx_pkt_rate_kpps = delta_pkts / ONE_THOUSAND;
@@ -167,7 +167,7 @@ static void print_tx_flow_line(uint64_t delta_bytes, uint64_t delta_pkts) {
             << " Mpps  ";
 
   std::cout << std::endl;
-}
+}*/
 
 static void print_tx_stats_line(uint64_t nb_bytes, uint64_t nb_pkts,
                                 uint64_t delta_bytes, uint64_t delta_pkts) {
@@ -297,17 +297,17 @@ void show_rx_flow_stats(const std::vector<uint64_t>& flow_stats,
     total_delta_pkts = total_nb_pkts_after - total_nb_pkts_before;
     total_delta_batches = total_nb_batches_after - total_nb_batches_before;
 
-    for (uint16_t i = 0; i < flow_stats_size; ++i) {
+    /*for (uint16_t i = 0; i < flow_stats_size; ++i) {
       uint64_t nb_pkts = nb_pkts_after[i];
       uint64_t delta_pkts = nb_pkts - nb_pkts_before[i];
 
       std::cout << "  Flow " << i << ":";
       std::cout << "    " << delta_pkts / ONE_THOUSAND << std::endl;
-    }
+    }*/
     print_stats_line(rx_stats->recv_bytes, rx_stats->nb_batches,
                      rx_stats->nb_pkts, total_delta_bytes, total_delta_pkts,
                      total_delta_batches);
-    std::cout << std::endl;
+    // std::cout << std::endl;
   }
 }
 
@@ -417,25 +417,25 @@ void show_tx_flow_stats(const std::vector<tx_stats_t>& tx_flows,
     uint64_t total_delta_bytes = total_nb_bytes_after - total_nb_bytes_before;
     uint64_t total_delta_pkts = total_nb_pkts_after - total_nb_pkts_before;
 
-    for (uint16_t i = 0; i < flows_size; ++i) {
+    /*for (uint16_t i = 0; i < flows_size; ++i) {
       uint64_t nb_bytes = nb_bytes_after[i];
       uint64_t nb_pkts = nb_pkts_after[i];
 
       uint64_t delta_bytes = nb_bytes - nb_bytes_before[i];
       uint64_t delta_pkts = nb_pkts - nb_pkts_before[i];
 
-      if (flows_size > 1) {
-        std::cout << "  Flow " << i << ":";
-        std::cout << "    ";
-        print_tx_flow_line(delta_bytes, delta_pkts);
-      }
-    }
+      // if (flows_size > 1) {
+        // std::cout << "  Flow " << i << ":";
+        // std::cout << "    ";
+        // print_tx_flow_line(delta_bytes, delta_pkts);
+      // }
+    }*/
     print_tx_stats_line(total_nb_bytes, total_nb_pkts, total_delta_bytes,
                         total_delta_pkts);
 
-    if (flows_size > 1) {
-      std::cout << std::endl;
-    }
+    // if (flows_size > 1) {
+    //   std::cout << std::endl;
+    // }
   }
 }
 
